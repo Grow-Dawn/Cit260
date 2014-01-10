a34ffafdb4478faded7b9230042b4ebedce0edd8
photos plus <photos plus@photosplus-PC.netgear.com>
2014-01-10 15:40:13 -0800

    Gallons to liters

---
+++ src/galtolit1/GalToLit1.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package galtolit1;
+
+/**
+ *
+ * @author Dawn Grow
+ */
+public class GalToLit1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+		double gallons;//holds the number of gallons
+                double liters; // holds conversion to liters
+		
+		gallons = 10; // start with 10 gallons
+		liters = gallons * 3.7854; // convert to liters
+		System.out.println(gallons + "gallons is " + liters + "liters");
+    }
+    
+}
+
+
